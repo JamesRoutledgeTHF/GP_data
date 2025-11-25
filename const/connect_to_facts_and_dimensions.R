c3856c2862834b5883cc7823eecdad2208f15296
routledj <routledj@chi.swan.ac.uk>
2025-11-25 12:03:00 +0000

    Commit connection file

---
+++ const/connect_to_facts_and_dimensions.R
@@ -0,0 +1,13 @@
+library(odbc)
+library(DBI)
+
+con <- DBI::dbConnect(
+  odbc::odbc(),
+  Driver = "ODBC Driver 17 for SQL Server",
+  Server = "factsanddimensions.database.windows.net",
+  Database = "Health_Foundation_UserDB",
+  UID = "", #insert username
+  PWD = "", #insert password
+  Encrypt = "yes"
+  
+)
